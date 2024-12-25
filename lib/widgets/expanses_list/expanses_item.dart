ca7498bd871a8e44decb8273c9e28895918f1528
mod664h@gmail.com <mod664h@gmail.com>
2024-12-25 02:24:13 +0200

    marge

---
+++ lib/widgets/expanses_list/expanses_item.dart
@@ -0,0 +1,39 @@
+import 'package:expenses/modles/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpansesItem extends StatelessWidget {
+  const ExpansesItem({super.key, required this.expense});
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text('${expense.amount.toStringAsFixed(2)}\$'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcon[expense.category]),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(expense.formattedDate())
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
