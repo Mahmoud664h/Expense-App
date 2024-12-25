77bc08384dc0b84f4311efbf7fc3cbce390075ab
mahmoud <mod664h@gmail.com>
2024-12-25 03:11:16 +0200

    NewExpense restore

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,10 +1,95 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class NewExpense extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
   @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  final formatter = DateFormat.yMd();
+  DateTime? _selectedDate;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      label: Text('Amount'), prefixText: '\$'),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(_selectedDate == null
+                        ? 'No Date selected '
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                        onPressed: () async {
+                          final now = DateTime.now();
+                          final firstDate =
+                              DateTime(now.year - 1, now.month, now.day);
+                          final DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: now,
+                              firstDate: firstDate,
+                              lastDate: now);
+                          setState(() {
+                            _selectedDate = pickedDate;
+                          });
+                        },
+                        icon: const Icon(Icons.calendar_month))
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 24),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel')),
+              ElevatedButton(
+                  onPressed: () {
+                    log(_titleController.text);
+                    log(_amountController.text);
+                  },
+                  child: const Text('Save Expense'))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
